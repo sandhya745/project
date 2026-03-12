6a5e942716eaa1c255ee6d6539c34258d5a471ae
codesandhya <chaudharysandhya204@gmail.com>
2026-03-12 18:44:10 +0545

    added the function of help/documentation in admin dash

---
+++ resources/views/admin/settings/backup.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+
+@section('title', 'Backup / Restore')
+
+@section('content')
+<div class="p-6 bg-white rounded shadow">
+    <h1 class="text-2xl font-bold mb-4">Backup / Restore</h1>
+    <p>Here you can manage backups and restore your data.</p>
+</div>
+@endsection
---
+++ resources/views/admin/settings/newsletter.blade.php
@@ -0,0 +1,11 @@
+{{-- resources/views/admin/settings/newsletter.blade.php --}}
+@extends('layouts.admin')
+
+@section('title', 'Newsletter / Email')
+
+@section('content')
+<div class="p-6 bg-white rounded shadow">
+    <h1 class="text-2xl font-bold mb-4">Newsletter / Email Settings</h1>
+    <p>Manage newsletter subscriptions and emails here.</p>
+</div>
+@endsection
